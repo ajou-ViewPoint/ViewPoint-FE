cad1b70296416e70b4e27f6d4611e2da76f70feb
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2025-11-25 12:21:59 +0900

    FIX: remove unused naviagtion

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,10 +1,8 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import style from './Header.module.scss';
 import logo from '../../assets/ViewPoint.png';
 
 function Header() {
-    const navigate = useNavigate();
-
     const headerInfo = [
         { id: '법안', path: '/billlist' },
         { id: '위원회', path: '/committee' },
@@ -16,8 +14,8 @@ function Header() {
 
     return (
         <div className={style.header}>
-            <a className={style.logo} href="/">
-                <img src={logo} alt="ViewPoint Logo" className={style.logoImage} />
+            <a className={style.logo} href="/" aria-label="홈으로 이동">
+                <img src={logo} alt="뷰포인트 로고" className={style.logoImage} />
             </a>
             <nav className={style.header__buttonRail}>
                 {headerInfo.map((item) => (
@@ -28,23 +26,11 @@ function Header() {
                                 ? `${style.header__button} ${style.active}`
                                 : style.header__button
                         }
-                        key={item.id}
-                        onClick={() => navigate(item.path)}>
+                        key={item.id}>
                         {item.id}
                     </NavLink>
                 ))}
             </nav>
-
-            {/* <form className={style.header__searchBar}>
-                <input
-                    className={style.header__searchBar__inputArea}
-                    type="text"
-                    aria-label="검색"
-                    placeholder="검색어를 입력하세요"></input>
-                <button className={style.header__searchBar__searchButton} type="submit">
-                    <Search />
-                </button>
-            </form> */}
         </div>
     );
 }
