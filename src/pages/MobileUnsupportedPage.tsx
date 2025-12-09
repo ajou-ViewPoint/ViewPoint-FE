9f92217e9d977c139207d2956da277363cd49125
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2025-12-09 14:21:03 +0900

    FEAT: improve mobile support page styling and enhance text readability

--- src/pages/MobileUnsupportedPage.tsx
+++ src/pages/MobileUnsupportedPage.tsx
@@ -9,9 +9,8 @@ function MobileUnsupportedPage() {
             <div className={style.card}>
                 <h1 className={style.title}>모바일 버전은 준비 중이에요</h1>
                 <p className={style.desc}>
-                    ViewPoint는 지금 데스크톱 환경에 최적화되어 있어요. <br />더 좋은 모바일 경험을
-                    제공하기 위해 열심히 준비 중입니다. <br />
-                    조금만 기다려 주세요!
+                    ViewPoint는 지금 데스크톱 환경에 최적화되어 있어요. 더 좋은 모바일 경험을
+                    제공하기 위해 열심히 준비 중입니다. 조금만 기다려 주세요!
                 </p>
                 <button className={style.action} onClick={() => navigate('/')}>
                     홈으로 돌아가기
