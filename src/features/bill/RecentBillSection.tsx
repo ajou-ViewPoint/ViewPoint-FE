3fd4e2e4aa7e598c9c6b09acc61b4a006e7f6684
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2025-12-10 15:59:18 +0900

    FEAT: update RecentBillSection and IdeologySection for improved title accuracy

--- src/features/bill/RecentBillSection.tsx
+++ src/features/bill/RecentBillSection.tsx
@@ -15,8 +15,8 @@ function RecentBillSection() {
         <div className={style.container}>
             <div className={style.header}>
                 <p className={style.header__number}>02.</p>
-                <h2 className={style.header__title}>최근 처리된 의안 한눈에 보기</h2>
-                <p className={style.header__discription}>최근 처리된 의안들을 확인해보세요.</p>
+                <h2 className={style.header__title}>최근 발의된 의안 한눈에 보기</h2>
+                <p className={style.header__discription}>최근 발의된 의안들을 확인해보세요.</p>
             </div>
             <div className={style.contents}>
                 {recentBillList.map((bill) => (
--- src/features/ideology/IdeologySection.tsx
+++ src/features/ideology/IdeologySection.tsx
@@ -14,7 +14,7 @@ function IdeologySection() {
         <div className={style.container}>
             <div className={style.header}>
                 <p className={style.header__number}>03.</p>
-                <h2 className={style.header__title}>제22대 국회의원 이념 공간 beta</h2>
+                <h2 className={style.header__title}>제{nominateAge}대 국회의원 이념 공간 beta</h2>
                 <p className={style.header__discription}>
                     국회의원들의 이념 분포를 한눈에 확인하세요.
                 </p>
